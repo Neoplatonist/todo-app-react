cc9bd1ed702a3dd68b64cf523090d0a1ef8045cf
Neoplatonist <mr.neoplatonist@gmail.com>
2020-10-18 17:52:42 +0900

    added main component for main content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 
 import Header from './layout/Header/index.js'
+import Main from './layout/Main'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
